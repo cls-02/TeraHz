045f0fc72f247ccfc3c6241743b718e232b0792d
d3m1g0d <kristjan.komlosi@gmail.com>
2019-01-09 21:30:20 +0100

    wrote a working parser

--- utils/getcdata.py
+++ utils/getcdata.py
@@ -10,17 +10,18 @@ uartpath = '/dev/ttyUSB0'
 uartbaud = 115200
 uarttout = 5
 
+wl = [410, 435, 460, 485, 510, 535, 560, 585, 610, 645, 680, 705, 730, 760, 810, 860, 900, 940]
+responseorder = [i for i in 'RSTUVWGHIJKLABCDEF'] # works, do NOT touch!
+realorder = [i for i in 'ABCDEFGHRISJTUVWKL']
+
 print('getcdata')
 print('This utility is part of the TeraHz project')
 
-wavelens = pd.Series([410, 435, 460, 485, 510, 535, 560, 585, 610, 645, 680, 705, 730, 760, 810, 860, 900, 940],
-                    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'R', 'I', 'S', 'J', 'T', 'U', 'V', 'W', 'K', 'L'])
+wavelens = pd.Series(realorder)
 
 wavetable = pd.DataFrame(columns=wavelens)
 
 with ser.Serial(uartpath, uartbaud, timeout=uarttout) as sensor:
     sensor.write(b'ATCDATA\n')
-    response = sensor.readline()
-    print(response.decode())
-    parsed = parse('{:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f}, {:f} OK', response.decode())
-    print(parsed)
+    response = pd.Series([float (i) for i in sensor.readline().decode()[:-3].split(',')]) # works, do not touch!
+    print(response)
