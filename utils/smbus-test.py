befbf04cd2e4b812c9fecc79ec53c4f00b51bcf8
TeraHz User <kristjan.komlosi@gmail.com>
2019-02-20 18:11:59 +0000

    SMBus access working!

--- utils/smbus-test.py
+++ utils/smbus-test.py
@@ -1,4 +1,9 @@
 import smbus2
 
 bus = smbus2.SMBus(1)
-print(bus.read_word_data(0x10, 0x07))
+
+result = bus.read_byte_data(0x39, 0x8a)
+print('LUX Meter ID = {}'.format(result))
+
+result = bus.read_word_data(0x10, 0x0c)
+print('UV sensor ID = {}'.format(result))
