44a636010efaef7d021587367dc0a20def38a985
d3m1g0d <kristjan.komlosi@gmail.com>
2019-03-31 11:09:44 +0200

    prototyping

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,10 @@ from flask import Flask, redirect, url_for, request
 import sensors
 import json
 app = Flask(__name__)
-s=sensors.Spectrometer(path='/dev/ttyACM0', baudrate=115200, tout=1)
+s=sensors.Spectrometer(path='/dev/ttyAMA0', baudrate=115200, tout=1)
+u=sensors.UVSensor()
+l=sensors.LxMeter()
 
 @app.route('/data')
 def sendData():
-    return json.dumps(s.getData())
+    return json.dumps([s.getData(), l.getData(), u.getABI])
