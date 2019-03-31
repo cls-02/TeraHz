3c55e96341a1789725cbe12889f2c7cc30771b4a
d3m1g0d <kristjan.komlosi@gmail.com>
2019-03-31 11:44:38 +0200

    fix

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, request
 import sensors
 import json
 app = Flask(__name__)
-s=sensors.Spectrometer(path='/dev/ttyAMA0', baudrate=115200, tout=1)
+s=sensors.Spectrometer(path='/dev/serial0', baudrate=115200, tout=1)
 u=sensors.UVSensor()
 l=sensors.LxMeter()
 
