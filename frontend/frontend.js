2e537840026dddb15df13bb4c765aa3a61299164
d3m1g0d <kristjan.komlosi@gmail.com>
2019-04-06 15:24:07 +0200

    fixed wrong url in frontend

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,7 +1,7 @@
 $('#update').click(updateData())
 function updateData () {
-  const url = 'http:' + window.location.hostname + ':5000/data'
+  const url = 'http://' + window.location.hostname + ':5000/data';
   $.get(url, function (data, status) {
-    $('#debug').text(data)
+    $('#debug').text(data);
   })
 }
