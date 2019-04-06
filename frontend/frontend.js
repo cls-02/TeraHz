fe07f41425d66d709cc4279891b64a05da0f3e2f
d3m1g0d <kristjan.komlosi@gmail.com>
2019-04-06 15:51:02 +0200

    table parsing test no 1

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -3,5 +3,8 @@ function updateData () {
   const url = 'http://' + window.location.hostname + ':5000/data';
   $.get(url, function (data, status) {
     $('#debug').text(data);
+    for (i in data[0]) {
+      $(i).text(data[0][i]);
+    }
   })
 }
