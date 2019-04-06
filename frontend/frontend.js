4b7f96a378f9363f964794e9f4a19424d903f757
d3m1g0d <kristjan.komlosi@gmail.com>
2019-04-06 15:47:29 +0200

    typo fix

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,4 +1,4 @@
-$('#update').click(updateData())
+$('#update').click(updateData;
 function updateData () {
   const url = 'http://' + window.location.hostname + ':5000/data';
   $.get(url, function (data, status) {
