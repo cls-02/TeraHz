37db23a0e8520a6eed5521e5d3129a4b49b9bd3a
d3m1g0d <kristjan.komlosi@gmail.com>
2019-04-14 20:38:43 +0200

    Divide and conquer on updateData...

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,10 +1,19 @@
 $('#update').click(updateData);
-function updateData () {
+// jQuery event binder
+
+function updateData (obj) {
+  // download data from backend into obj
   const url = 'http://' + window.location.hostname + ':5000/data';
-  $.get(url, function (data, status) {
-    $('#debug').text(data);
-    for (const i in data[0]) {
-      $('#'+i).text(data[0][i]);
-    }
+  // I understand how bad this line looks. Please don't judge me...
+  $.get(url, function (data, status) { // standard jQuery AJAX
+    obj = data;
   });
+}
+
+function applyData (obj, dom) {
+  // applies data in obj[0] to HTML tags with the obj's key as ID.
+  // useful mostly for slapping spectrometer JSON into HTML tables.
+  for (var i in obj[0]) {
+    $(dom).find('#' + i).text(obj[0][i]);
+  }
 }
