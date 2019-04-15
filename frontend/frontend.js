52464ec5a82e811d2b38102bf22b04ad2d13c9fe
d3m1g0d <kristjan.komlosi@gmail.com>
2019-04-15 17:43:34 +0200

    added inital chart.js support

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,4 +1,9 @@
-$('#update').click(updateData);
+var globalObject;
+$('#update').click(function () {
+  updateData(globalObject);
+  fillTable(globalObject, $('table'));
+  graphSpectralData(globalObject, $('#spectrogram'));
+});
 // jQuery event binder
 
 function updateData (obj) {
@@ -10,10 +15,24 @@ function updateData (obj) {
   });
 }
 
-function applyData (obj, dom) {
+function fillTable (obj, dom) {
   // applies data in obj[0] to HTML tags with the obj's key as ID.
   // useful mostly for slapping spectrometer JSON into HTML tables.
   for (var i in obj[0]) {
     $(dom).find('#' + i).text(obj[0][i]);
   }
+}
+
+function graphSpectralData(obj, dom) {
+  // graphs the data from obj[0] into canvas at dom
+  var chart = new Chart(dom, {
+    type: 'line',
+    data: {
+      labels: obj[0].keys(),
+      datasets: [{
+        label: 'Spectrometer data',
+        data: obj[0].values()
+      }]
+    }
+  });
 }
