f0d1fe23017e2c9a6c4f26d3275fe815d7b118b8
Kristjan Komlosi <kristjan.komlosi@gmail.com>
2019-11-09 19:51:26 +0100

    install and setup scripts

--- etcs/install.sh
+++ etcs/install.sh
@@ -3,7 +3,18 @@ apt -y update
 apt -y full-upgrade
 apt install -y python3 python3-pip lighttpd dnsmasq hostapd libatlas-base-dev
 pip3 install numpy pandas flask smbus2 pyserial
+
 cp -R hostapd/ /etc
 cp -R lighttpd/ /etc
 cp dnsmasq.conf /etc
+cp rc.local /etc
 cp -R ../frontend /var/www/html
+cd /etc/lighttpd
+chmod +x edit_ssid.sh
+./edit_ssid
+
+mkdir -p /usr/local/lib/TeraHz
+cp -R ../backend/* /usr/local/lib/terahz
+
+systemctl unmask dnsmasq hostapd lighttpd
+systemctl enable dnsmasq hostapd lighttpd
