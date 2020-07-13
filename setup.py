ac154e677033e39c7253d22c5e4b3969a9fe9ed0
Kristjan Komlosi <kristjan.komlosi@gmail.com>
2020-07-13 13:28:32 +0200

    moved templates

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="terahz", # Replace with your own username
+    version="0.0.1",
+    author="Kristjan Komloši",
+    author_email="kristjan.komlosi@gmail.com",
+    description="Low cost spectrometry webapp",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cls-02/TeraHz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    )
