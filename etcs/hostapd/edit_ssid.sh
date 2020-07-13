54b93cb05c6bc2459fb64c7176d45df98a40642a
Kristjan Komlosi <kristjan.komlosi@gmail.com>
2020-07-13 13:11:10 +0200

    cleaning up

--- etcs/hostapd/edit_ssid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# edit_ssid.sh - edits hostapd.conf and sets a MAC address-based SSID
-cd `dirname $0`
-ssid=`ip link | awk '/wlan0/ {getline; print $2}' | awk -v FS=':' '{printf("TeraHz_%s%s%s\n", $4, $5, $6)}'`
-sed "/ssid=.*/s/ssid=.*/ssid=$ssid/" hostapd.conf > newconf
-mv newconf hostapd.conf
