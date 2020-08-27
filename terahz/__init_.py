97233f27c319e106ec5147149dc70da6648fe92f
Kristjan Komlosi <kristjan.komlosi@gmail.com>
2020-08-27 11:14:01 +0200

    fixing packaging #1

--- terahz/__init_.py
+++ terahz/__init_.py
@@ -2,17 +2,18 @@
 '''Main TeraHz backend program'''
 # All code in this file is licensed under the ISC license, provided in LICENSE.txt
 from flask import Flask, jsonify
-import .terahz
+from . import terahz
 
-app = Flask(__name__)
-
+def start_flaskapp():
+    global app, s, u, l
+    app = Flask(__name__)
+    s = terahz.Spectrometer(path='/dev/serial0')
+    u = terahz.UVSensor()
+    l = terahz.LxMeter()
 
 @app.route('/data')
 def sendData():
     '''Responder function for /data route'''
-    s = sensors.Spectrometer(path='/dev/serial0')
-    u = sensors.UVSensor()
-    l = sensors.LxMeter()
     response = jsonify([s.getData(), l.getData(), u.getABI()])
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
